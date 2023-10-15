2b36d34c68fe13ce24b3c834d2dc745b1638d15f
SupercraftD <dzhu700@gmail.com>
2023-10-15 10:21:26 -0400

    add select dropdown menu for contact

--- contact.js
+++ contact.js
@@ -1,4 +1,4 @@
-schools = {
+let schools = {
     "Argyle MS":"https://www2.montgomeryschoolsmd.org/schools/argylems/staff/directory#Guidance/Counseling",
     "Cabin John MS":"https://www2.montgomeryschoolsmd.org/schools/cabinjohnms/staff/directory/#Counseling_Office",
     "William H. Farquhar MS":"https://www2.montgomeryschoolsmd.org/schools/farquharms/staff/directory/#Guidance",
@@ -48,6 +48,42 @@ schools = {
     "James Hubert Blake HS":"https://www2.montgomeryschoolsmd.org/schools/blakehs/staff/directory/#Counseling",
     "Thomas Edison HS":"https://www2.montgomeryschoolsmd.org/schools/edison/about/directory/#Counseling",
     "Walter Johnson HS":"https://www2.montgomeryschoolsmd.org/schools/wjhs/staff/staff-directory/?processlevel=04424#Counseling_Services",
-    "Winston Churchill HS":"https://www2.montgomeryschoolsmd.org/schools/wjhs/staff/staff-directory/?processlevel=04424#Counseling_Services",
-    
+    "Winston Churchill HS":"https://www2.montgomeryschoolsmd.org/schools/churchillhs/staff/directory/#Counseling_Department",
+    "John F. Kennedy HS":"https://www2.montgomeryschoolsmd.org/schools/kennedyhs/staff/directory/#Counseling",
+    "Northwest HS":"https://www2.montgomeryschoolsmd.org/schools/northwesths/staff/directory/#Counseling_Services",
+    "Poolesville HS":"https://www2.montgomeryschoolsmd.org/schools/poolesvillehs/staff/directory/#Counseling_Services",
+    "Col. Zadok Magruder HS":"https://www2.montgomeryschoolsmd.org/schools/magruderhs/staff/directory/#Counseling_Services",
+    "Northwood HS":"https://www2.montgomeryschoolsmd.org/schools/northwoodhs/about/staff/",
+    "Quince Orchard HS":"https://www2.montgomeryschoolsmd.org/schools/qohs/staff/directory/#Counseling",
+    "Richard Montgomery HS":"https://www2.montgomeryschoolsmd.org/schools/rmhs/staff/directory/#Counseling_Department",
+    "Paint Branch HS":"https://www2.montgomeryschoolsmd.org/schools/paintbranchhs/staff/directory/#Counseling_Department",
+    "Rockville HS":"https://www2.montgomeryschoolsmd.org/schools/rockvillehs/staff/directory/#Counseling",
+    "Springbrook HS":"https://www2.montgomeryschoolsmd.org/schools/springbrookhs/staff/directory/#Counseling",
+    "Walt Whitman HS":"https://docs.google.com/document/u/1/d/e/2PACX-1vTD_Um51-AX1ZUDaF2Ht-jES8vDD9KLn4qNG8z3f4iqrEmwSQmUlbE_WFbJVglR3JkacrZF6F5Djzcl/pub#h.6basohj5mpn9",
+    "Seneca Valley HS":"https://www2.montgomeryschoolsmd.org/schools/senecavalleyhs/staff/directory/#Counseling_Services",
+    "Watkins Mill HS":"https://www2.montgomeryschoolsmd.org/schools/watkinsmillhs/staff/directory/#Counseling",
+    "Thomas S. Wootton HS":"https://www2.montgomeryschoolsmd.org/schools/woottonhs/staff/directory/#Counseling_Services",
+    "Sherwood HS":"https://www2.montgomeryschoolsmd.org/schools/sherwoodhs/staff/directory/#Counseling",
+    "Wheaton HS":"https://www2.montgomeryschoolsmd.org/schools/wheatonhs/admin/directory/#Counseling"
+}
+
+for (let school in schools){
+    document.getElementById("schoolselect").innerHTML += `<option value="${school}">${school}</option>`
+}
+window.onload = function(e){
+    let val = document.getElementById('schoolselect').value
+
+    document.getElementById('schoollink').href = schools[val]
+    document.getElementById('schoollink').innerHTML = schools[val]
+    console.log(schools[val])
+
+}
+document.getElementById('schoolselect').oninput = function(e){
+    let val = document.getElementById('schoolselect').value
+
+    document.getElementById('schoollink').href = schools[val]
+
+    document.getElementById('schoollink').innerHTML = schools[val]
+    console.log(schools[val])
+
 }
